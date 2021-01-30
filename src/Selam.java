9d402874b29fba311973a7f97b1e1d6996766295
simgeay <ay.simge@gmail.com>
2021-01-30 12:45:32 +0100

    Initial commit

---
+++ src/Selam.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Selam {
+    String world = "millet";
+
+    public String selamSoyle(String kime) {
+        String cumle;
+        if (kime != ""){
+            cumle = "Selam " + kime + " :)";
+        }
+        else
+            cumle = "Selam " + world + " :)";
+        return cumle;
+    }
+}
---
+++ src/SelamTest.java
@@ -0,0 +1,7 @@
+public class SelamTest {
+    public static void main(String[] args){
+        Selam nesne = new Selam();
+        String cevap = nesne.selamSoyle("Simge");
+        System.out.println(cevap);
+    }
+}
